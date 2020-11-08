a9279168d0d9ee9b7d2b78adbcd23b9750c33e24
shk3029 <shk30290@naver.com>
2020-11-08 22:09:23 +0900

    Initial commit

---
+++ src/test/java/me/js/lucene/LuceneApplicationTests.java
@@ -0,0 +1,13 @@
+package me.js.lucene;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LuceneApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
